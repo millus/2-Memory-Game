fae854bb6de9d05f52ef251685d0f06f54f8c91b
Camilla Knutsen <camillaknutsen@Camillas-MacBook-Pro-2.local>
2019-08-20 13:51:13 +0200

    Made first version of the grid of cards.

---
+++ js/app.js
@@ -0,0 +1,12 @@
+/*
+TODO: Code tested in browser, and worked for traversing cards with for...of
+const cards = document.querySelectorAll('.card');
+undefined
+cards
+NodeList(16) [li.card, li.card, li.card, li.card, li.card, li.card, li.card, li.card, li.card, li.card, li.card, li.card, li.card, li.card, li.card, li.card]0: li.card1: li.card2: li.card3: li.card4: li.card5: li.card6: li.card7: li.card8: li.card9: li.card10: li.card11: li.card12: li.card13: li.card14: li.card15: li.cardlength: 16__proto__: NodeList
+for (const card of cards){}
+undefined
+for (const card of cards){ if (card.textContent === '3'){card.style.backgroundColor ='red'}else{card.style.backgroundColor = 'blue';}}
+"blue"
+
+*/
