edcd2f0a6636f9635060d6f0a710d34d1694ea0f
Camilla Knutsen <camillaknutsen@Camillas-MacBook-Pro-2.local>
2019-08-21 10:48:16 +0200

    feat: added shuffle method, cards now appear shuffled every refresh.
    Just read github styleguide. So trying to do it more correctly.

--- js/app.js
+++ js/app.js
@@ -8,28 +8,55 @@ for (const card of cards){}
 undefined
 for (const card of cards){ if (card.textContent === '3'){card.style.backgroundColor ='red'}else{card.style.backgroundColor = 'blue';}}
 "blue"
-
 */
 
 /* TODO: Add real symbols here,
  * and update function to have 2-sided cards. */
-const symbols = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+let symbols = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 const container = document.querySelector('.card-container');
 
+symbols = shuffle(symbols);
+addCardsToContainer(symbols, container);
 
 /* Method for creating the cards and adding them to the DOM with symbol as content */
-for(const symbol of symbols){
-  const card = document.createElement("li"); /* makes <li></li> */
-  const cardBox = document.createElement("div");
-  const cardUp = document.createElement("div");
-  const cardDown = document.createElement("div");
-  card.setAttribute('class', 'card');
-  cardUp.setAttribute('class','card-front'); /* makes <li class="card card-front"> </li> */
-  cardDown.setAttribute('class','card-back');
-  cardBox.setAttribute('class', 'card-box');
-  container.appendChild(card);
-  card.appendChild(cardBox);
-  cardBox.appendChild(cardUp); /* adds the new <li> to existing <ul> */
-  cardBox.appendChild(cardDown);
-  cardDown.textContent = `${symbol}`; /* inserts the symbol in the <li> */
+function addCardsToContainer(symbols, container) {
+  for(const symbol of symbols){
+    const card = document.createElement("li"); /* makes <li></li> */
+    const cardBox = document.createElement("div");
+    const cardUp = document.createElement("div");
+    const cardDown = document.createElement("div");
+    card.setAttribute('class', 'card');
+    cardBox.setAttribute('class', 'card-box');
+    cardUp.setAttribute('class','card-front'); /* makes <li class="card card-front"> </li> */
+    cardDown.setAttribute('class','card-back');
+    container.appendChild(card);
+    card.appendChild(cardBox);
+    cardBox.appendChild(cardUp); /* adds the new <li> to existing <ul> */
+    cardBox.appendChild(cardDown);
+    cardDown.textContent = `${symbol}`; /* inserts the symbol in the <li> */
+  }
+}
+
+/*
+ * The de-facto unbiased shuffle algorithm is the Fisher-Yates (aka Knuth) Shuffle.
+ * See https://github.com/coolaj86/knuth-shuffle
+ * Ref: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array/2450976#2450976
+ */
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
 }
